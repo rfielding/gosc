37afc20c0035058a5a6c0a60539faa0fe592c915
Rob Fielding <rob.fielding@gmail.com>
2013-07-13 02:03:04 -0400

    add in UDP basic

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ gosc
 A simple project for doing audio generation with Go, starting under Linux.
 The first steps are simple.
 
-Build the binary, and it will emit CD standard audio on stdout.  Redirect it into a command-line program.  So with the built gosc program, do this to emit audio:
+Build the binary, and it will emit CD standard audio on stdout.  Redirect it into a command-line program.  So with the built gosc program, do this to emit audio while listening on UDP port 9999 from anywhere:
 
-./gosc | aplay -f cd
+./gosc 0.0.0.0:9999 | aplay -f cd
 
 For now, it's a simple stereo tone to smoke-test the idea.  Todo is to add in listening on UDP port (preferably the SuperCollider standard port).  From there I will listen for OSC packets in a well-defined format that's emitted by my Windows 8 project (CSharpAttempt).
--- gosc.go
+++ gosc.go
@@ -1,26 +1,58 @@
 package main
 
 import (
+    "flag"
     "bytes"
     "encoding/binary"
-//    "fmt"
-//    "bufio"
     "os"
     "math"
+    "net"
 )
 
-func main() {
+type OscResponse struct {
+  Voice int32
+  Vol float32
+  Pitch float32
+}
+
+func oscListen(responseChannel chan<-OscResponse, sock *net.UDPConn) {
+    //var udpBuf [1024]byte
+    //TODO: do something with incoming UDP packets from controller
+}
+
+func generateNoise(responseChannel chan<-OscResponse) {
     var t float64 = 0
     file := os.Stdout
     data := new(bytes.Buffer)
     for {
+        //TODO: check for UDP packets and send data in response
         data.Reset()
-        l := (int16) ( 30000 * math.Sin(t) )
-        r := (int16) ( 30000 * math.Cos(2*t) )
+        l := (int16) ( 16000 * math.Sin(t) )
+        r := (int16) ( 16000 * math.Cos(2*t) )
         binary.Write(data, binary.LittleEndian, l)
         binary.Write(data, binary.LittleEndian, r)
         file.Write(data.Bytes())
         file.Sync()
         t = t + 0.01
+    }
+}
+
+func main() {
+    flag.Parse()
+    args :=  flag.Args()
+
+    //Start listening on our UDP port and make noise in response
+    addr, err := net.ResolveUDPAddr("udp",args[0])
+    if err == nil {
+        sock, erru := net.ListenUDP("udp",addr)
+        if erru == nil {
+            responseChannel := make(chan OscResponse)
+            go oscListen(responseChannel,sock)
+            generateNoise(responseChannel)
+        } else {
+            os.Stderr.WriteString("could not listen")
+        }
+    } else {
+      os.Stderr.WriteString("could not open address")
     }
 }
